48323105bcfdc5f1febe4eaedff8aedc0f8b06aa
Rodrigo Moreira <hola@piducan.dev>
2021-04-08 18:39:11 -0400

    add buffer param

--- api/demo.js
+++ api/demo.js
@@ -7,6 +7,15 @@ const tts = new TextToSpeechV1({
   disableSslVerification: true,
 });
 
+function streamToString(stream) {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on("error", (err) => reject(err));
+    stream.on("end", () => resolve(Buffer.concat(chunks)));
+  });
+}
+
 module.exports = async (req, res) => {
   try {
     const { result } = await tts.synthesize({
@@ -14,7 +23,12 @@ module.exports = async (req, res) => {
       accept: "audio/mp3",
       voice: req.query.voice || "en-US_MichaelV3Voice" /* For spanish use "es-LA_SofiaV3Voice" */,
     });
-    result.pipe(res);
+    if (req.query.buffer) {
+      const buffer = await streamToString(result);
+      res.send(buffer);
+    } else {
+      result.pipe(res);
+    }
   } catch (error) {
     res.status(500).json({ error: JSON.stringify(error) });
   }
